9ea375c83393ea68fd3d693d30b9b771becd8ca6
Andrey Burtasov <a.burtasov@yarus.ru>
2022-07-18 22:50:46 +0300

    added files

---
+++ src/WhileLoops.java
@@ -0,0 +1,12 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+       // boolean t = true; // 5 > 2 // 5==5 // 5 >= 5
+        int value = 0;
+        //boolean b = value > 5;
+        while(value < 5) {
+        System.out.println("Hello " + value);
+        value = value + 1;
+        }
+        
+    }
+}
