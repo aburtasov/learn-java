99686a6f5682ceab17b437460630978f16b17274
Andrey Burtasov <a.burtasov@yarus.ru>
2022-07-18 22:08:53 +0300

    added strings

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        int x = 5; // примитивные типы данных с маленькой буквы
+
+        String s = "Hello"; // строка это класс ... переменная s сылается на объект "Hello"
+        String space = " ";
+        String name = "Bob";
+
+        System.out.println(s + space + name);
+        System.out.println("Hello" + " " + "John");  // sout + tab
+        System.out.println("My numbers is " + x);     
+    }
+}
--- src/Variable.java
+++ src/Variable.java
@@ -2,6 +2,16 @@ public class Variable {
     public static void main(String[] args) {
         int myInt;
         myInt = 10;
+        //short myShort = 3326;
+        //long myLong = 254654645;
+
+        //double myDouble = 235.35;
+        //float myFloat = 2362.4f;
+
+        //char c = 'a';
+       // boolean b = true;
+
+        //byte myByte = 100;  // -128 128 
         System.out.println(myInt);
     }
 }
