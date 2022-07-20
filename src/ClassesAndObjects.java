8ef13593b05b14271a4e7e426cda7487705e8290
Andrey Burtasov <a.burtasov@yarus.ru>
2022-07-20 03:30:30 +0300

    added methods

--- src/ClassesAndObjects.java
+++ src/ClassesAndObjects.java
@@ -8,8 +8,13 @@ public class ClassesAndObjects {
      person2.name = "Vova";
      person2.age = 21;
 
-     System.out.println("Name: " + person1.name + "Age: " + person1.age);
-     System.out.println("Name: " + person2.name + "Age: " + person2.age);
+     //System.out.println("Name: " + person1.name + " Age: " + person1.age);
+     //System.out.println("Name: " + person2.name + " Age: " + person2.age);
+     person1.speak();
+     person2.speak();
+     person1.sayHello();
+     int years1 = person1.calculateYearsToRetirement();
+     System.out.println(years1);
   }
 }  // публичный класс может быть только один
 /*class test {
@@ -25,4 +30,17 @@ class Person {
     // 2. Действия(методы)
     String name;
     int age;
+
+    void speak() {
+      System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    void sayHello() {
+      System.out.println("Hello from " + name);
+    }
+
+    int calculateYearsToRetirement() {
+      int years = 65 - age;
+      return years;
+    }
 }
