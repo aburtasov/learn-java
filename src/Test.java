b66ffc251c76b30efbbe51357ff92dfb27e86fac
Andrey Burtasov <a.burtasov@yarus.ru>
2022-07-22 10:52:44 +0300

    added packages

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import Forest.Squirell;
+import Forest.Tree;
+import Forest.Something.Some;
+//import Forest.*;
+
+
+public class Test {
+     public static void main(String[] args) {
+       Tree tree1 = new Tree();
+       Squirell squirell1 = new Squirell();
+
+       Scanner scan = new Scanner(System.in);
+
+       Some some1 = new Some();
+     }
+        
+}
